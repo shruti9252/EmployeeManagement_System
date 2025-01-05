f42babc573ebbafaf194e28393b48d50f0215d56
shruti9252 <shrutichatarkar@gmail.com>
2025-01-05 21:25:07 +0530

    first commit

---
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../other/Header'
+import CreateTask from '../other/CreateTask'
+import AllTasks from '../other/AllTasks'
+const AdminDashboard = (props) => {
+    return (
+        <div className='h-screen w-full p-7'>
+            <Header changeUser={props.changeUser} />
+            <CreateTask />
+          <AllTasks/>
+        </div>
+    )
+}
+
+export default AdminDashboard
